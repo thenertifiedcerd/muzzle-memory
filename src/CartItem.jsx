1aa53f1ec61edfe6c4ffe6585c3a6209a63a5f47
thenertifiedcerd <benzenefemiakinwande@gmail.com>
2024-12-31 10:10:18 -0500

    baaa

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,7 +9,9 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    return items.reduce((total, item) => {
+        return total + (item.quantity * item.cost);
+      }, 0);
   };
 
   const handleContinueShopping = (e) => {
